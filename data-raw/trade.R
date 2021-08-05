1aaa0dbaacc0082f58dcedf006ff63ac8e867464
Diego Ferreras Garrucho <d.ferreras-garrucho@lse.ac.uk>
2021-08-05 18:04:25 +0200

    Change selected provisions to iceberg

--- data-raw/trade.R
+++ data-raw/trade.R
@@ -10,6 +10,7 @@ WB_TRADE_DATA <- read_dta("/Users/diego/Documents/OneDrive - London School of Ec
 # Large provisions data
 WB_LARGE <- read_dta("/Users/diego/Documents/OneDrive - London School of Economics/CEP/Summer RA 2020/Files as of June 2021 (for Diego)/Data/temp_provisions_largedataset_essential_Jan302021.dta")
 
+
 # Merge:
 
 trade <- merge(WB_TRADE_DATA,WB_LARGE, c("iso1", "iso2", "year"), all.x = TRUE, all.y = FALSE)
@@ -33,12 +34,10 @@ trade$iso2  <- factor(trade$iso2)
 trade$year <- ordered(trade$year)
 colnames(trade)[1:3] <- c("exp", "imp", "time")
 
-# Finally, to facilitate testing, we drop most of the provision data and keep just 50 provisions chosen
-# at random:
+# Finally, to facilitate testing, we drop most of the provision data and keep just the iceberg provisions:
 
-set.seed(1)
-keep<-sample(10:314, size = 50, replace = FALSE)
-trade <- trade[, c(1:9, keep)]
+iceberg <- read.csv("/Users/diego/Documents/OneDrive - London School of Economics/CEP/Summer RA 2020/Files as of June 2021 (for Diego)/Data/icberg_provisions.csv", header = FALSE)
+trade <- trade[, c("exp", "imp", "time", "export", "id", "agreement", iceberg$V1[iceberg$V2 == 1])]
 
 # Save as .Rdata file, with compression to stay below CRAN's 5Mb size limit:
 
