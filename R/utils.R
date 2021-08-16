f8f15c7dbeed1c445ddd0de5f6c60b5929a8ed57
Diego Ferreras Garrucho <d.ferreras-garrucho@lse.ac.uk>
2021-08-16 11:31:50 +0200

    Remove unused setupLambda function

--- R/utils.R
+++ R/utils.R
@@ -1,30 +1,3 @@
-## !!! CURRENTLY NOT USING !!!!
-# setupLambda <- function(X, y, lambda.min, nlambda, penalty.factor) {
-#  n <- nrow(X)
-#  p <- ncol(X)
-
-#  ## Determine lambda.max
-#  ind <- which(penalty.factor!=0)
-#  if (length(ind)!=p) {
-#    fit <- glm(y~X[, -ind], family="gaussian")
-#  } else {
-#    fit <- glm(y~1, family=family)
-#  }
-
-#  zmax <- .Call("maxprod", X, fit$residuals, ind, penalty.factor) / n
-#  lambda.max <- zmax/alpha
-
-#  if (lambda.min==0) {
-#    lambda <- c(exp(seq(log(lambda.max), log(.001*lambda.max), len=nlambda-1)), 0)
-#  } else {
-#    lambda <- exp(seq(log(lambda.max), log(lambda.min*lambda.max), len=nlambda))
-#  }
-#
-#  if (length(ind)!=p) lambda[1] <- lambda[1] * 1.000001
-#  lambda
-#}
-
-
 #' Checking for Perfect Multicollinearity
 #'
 #' \code{collinearity_check} checks for perfect multicollinearity in a model with high-dimensional
