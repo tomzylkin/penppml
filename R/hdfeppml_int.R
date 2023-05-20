92109e692f7f899425ea775327cda98038f3bb16
tomzylkin <tomzylkin@gmail.com>
2023-05-20 14:19:04 -0400

    Update hdfeppml_int.R
    change collinearity check options

--- R/hdfeppml_int.R
+++ R/hdfeppml_int.R
@@ -78,8 +78,7 @@
 #' models with an application to gun control", \emph{Journal of Business & Economic Statistics}, 34, 590-605.
 #'
 
-hdfeppml_int <- function(y, x=NULL, fes=NULL, tol = 1e-8, hdfetol = 1e-4, colcheck_x = TRUE,
-                             colcheck_x_fes = TRUE, mu = NULL, saveX = TRUE,
+hdfeppml_int <- function(y, x=NULL, fes=NULL, tol = 1e-8, hdfetol = 1e-4,  mu = NULL, saveX = TRUE,
                              colcheck = TRUE, colcheck_x = colcheck, colcheck_x_fes = colcheck,
                              init_z = NULL, verbose = FALSE, maxiter = 1000, cluster = NULL, vcv = TRUE) {
   if(missing(x) & missing(fes)){
